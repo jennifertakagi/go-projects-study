a6617b07e0fea29479d00d93694cb840c15e465e
jennifertakagi <jennitakagi@gmail.com>
2020-10-28 10:10:41 -0300

    feat: add deck test file to project

---
+++ cards/deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 48 {
+		t.Errorf("Expected deck length of 48, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Clubs" {
+		t.Errorf("Expected first card of Ace of Clubs, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Spades" {
+		t.Errorf("Expected first card of King of Spades, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 48 {
+		t.Errorf("Expected deck length of 48, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
